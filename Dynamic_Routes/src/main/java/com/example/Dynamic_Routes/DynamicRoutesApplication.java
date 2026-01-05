7f8a10f995ab2f271e11879ee25d76eeaf73616d
Ayushmangiri <ayushmangiri@gmail.com>
2026-01-05 16:02:19 +0530

    first commit

---
+++ Dynamic_Routes/src/main/java/com/example/Dynamic_Routes/DynamicRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dynamic_Routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicRoutesApplication.class, args);
+	}
+
+}
---
+++ Dynamic_Routes/src/test/java/com/example/Dynamic_Routes/DynamicRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dynamic_Routes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
